bc2cbc68518234a95dc69bba6e8d7b3228a247b8
PujithaKonepalli <pujitha.konepalli@gmail.com>
2026-04-14 14:41:40 -0400

    Update db.ts

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 export const pool = new Pool({
   user: "postgres",
-  password: "3593", // replace
+  password: "0000", // replace
   host: "localhost",
   port: 5432,
   database: "crm",
